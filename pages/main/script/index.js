94231de0706802fbeb20aa2c1231fa284f23f454
skelegor8 <skelegor8@gmail.com>
2022-06-15 12:02:30 +0200

    fix: slider timer

--- pages/main/script/index.js
+++ pages/main/script/index.js
@@ -2,15 +2,21 @@
 let nextSliderBtn = document.querySelector(".next-btn");
 let prevSliderBtn = document.querySelector(".prev-btn");
 
+timerSlider(0);
+
 let slideIndex = 1; // Индекс слайда по умолчанию
 showSlides(slideIndex);
 
 function nextSlide() { //Функция увеличивает индекс на 1, показывает следующий слайд
     showSlides(slideIndex += 1);
+    clearInterval(sliderInterval);
+    timerSlider(2000);
 }
 
 function prevSlide() { //Функция уменьшает индекс на 1, показывает предыдущий слайд
-    showSlides(slideIndex -= 1);  
+    showSlides(slideIndex -= 1);
+    clearInterval(sliderInterval);
+    timerSlider(2000);
 }
 
 function currentSlide(n) { // Устанавливает текущий слайд
@@ -34,7 +40,10 @@ function showSlides(n) { //Функция для перелистывания с
 nextSliderBtn.addEventListener("click", nextSlide);
 prevSliderBtn.addEventListener("click", prevSlide);
 
-setInterval(nextSlide, 4000);
+function timerSlider(seconds) {
+    let sliderInterval = setInterval(nextSlide, 5000);
+    setTimeout(sliderInterval, seconds);
+}
 //----------- Слайдер -----------
 
 

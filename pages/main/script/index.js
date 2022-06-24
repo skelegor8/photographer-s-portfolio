9804102fd1671a07573c2ddbdb9dbc3ddc474527
skelegor8 <skelegor8@gmail.com>
2022-06-24 12:48:39 +0200

    feat: add gallery & album slider

--- pages/main/script/index.js
+++ pages/main/script/index.js
@@ -56,5 +56,18 @@ prevSliderBtn.addEventListener("click", prevSlide);
 
 //----------- Слайдер -----------
 
+let galleryItemLink = document.querySelectorAll('.gallery__item-link');
 
+
+galleryItemLink.forEach(galleryItemLink => {
+  galleryItemLink.addEventListener('click', () => {
+    let dataWeddings = galleryItemLink.dataset.weddings;
+    createUrl(dataWeddings);
+  })
+});
+
+function createUrl(dataWeddings) {
+  localStorage.clear;
+  localStorage.setItem("dataWeddings", dataWeddings);
+}
 

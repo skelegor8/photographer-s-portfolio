167e835b86b3c4565d5d5f9c7a3a99794b140d73
skelegor8 <skelegor8@gmail.com>
2022-06-29 15:06:07 +0200

    feat: add svipe to album slider

--- pages/gallery/album-page/index.js
+++ pages/gallery/album-page/index.js
@@ -28,7 +28,7 @@ async function populate() {
   imgAddUrl(imgUrls);
 }
 let slideIndex = 0;
-
+const screenWidth = document.documentElement.scrollWidth;
 function imgAddUrl(obj) {
   let indexAlbum = 1;
   for (let i = 0; i < obj[`${localStorage.getItem("dataWeddings")}`].length; i++) {
@@ -38,13 +38,20 @@ function imgAddUrl(obj) {
      if (indexAlbum == 1) {
        column_1.append(div);
        indexAlbum++;
+       if (screenWidth < 767) {
+        indexAlbum = 1;
+      }
       } else if(indexAlbum == 2) {
        column_2.append(div);
-       indexAlbum++;
-      } else if(indexAlbum == 3) {
+        if (screenWidth < 1023) {
+          indexAlbum = 1;
+        } else if (screenWidth > 1023) {
+          indexAlbum++;
+        }
+      } else if(indexAlbum == 3 & screenWidth > 1023) {
         column_3.append(div);
         indexAlbum++;
-       } else if(indexAlbum == 4) {
+       } else if(indexAlbum == 4 & screenWidth > 1023) {
         column_4.append(div);
         indexAlbum = 1;
        }
@@ -126,8 +133,49 @@ function imgAddUrl(obj) {
   }
   nextSliderBtn.addEventListener("click", nextSlide);
   prevSliderBtn.addEventListener("click", prevSlide);
-}
 
+  // Вешаем на прикосновение функцию handleTouchStart
+document.addEventListener('touchstart', handleTouchStart, false);  
+// А на движение пальцем по экрану - handleTouchMove      
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;                                                        
+
+function handleTouchStart(evt) {                                         
+    xDown = evt.touches[0].clientX;                                      
+    yDown = evt.touches[0].clientY;                                      
+};                                                
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+    // немного поясню здесь. Тут берутся модули движения по оси абсцисс и ординат (почему модули? потому что если движение сделано влево или вниз, то его показатель будет отрицательным) и сравнивается, чего было больше: движения по абсциссам или ординатам. Нужно это для того, чтобы, если пользователь провел вправо, но немного наискосок вниз, сработал именно коллбэк для движения вправо, а ни как-то иначе.
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+          nextSlide()
+        } else {
+          prevSlide()
+        }                       
+    } else { // Это вам, в общем-то, не надо, вы ведь только влево-вправо собираетесь двигать
+        if ( yDiff > 0 ) {
+            /* up swipe */ 
+        } else { 
+            closeSlider()
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
+}
 populate();
 
 
